f3ff9d0848dde8848061881f9388544d4b1c62a0
Jan--Henrik <janhenrik@janhenrik.org>
2019-06-17 03:54:08 +0200

    created IP as variable

--- capture.py
+++ capture.py
@@ -9,6 +9,8 @@ import time
 import sys
 
 outfile = open(sys.argv[1], 'w')
+hostIP = b"192.168.68.1"
+sensorIP = "192.168.68.64"
 
 #print_lock = threading.Lock()
 
@@ -39,7 +41,7 @@ def receive(recv):
             cmd2.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
             cmd2.bind(('0.0.0.0',50452))
             cmd2.connect((host, port))
-            cmd2.sendall(b"ID=000094DA367B\x0d\x0aIP=192.168.68.1\x0d\x0aPORT=50444\x0d\x0aCMD=CLOSE\x0d\x0a")
+            cmd2.sendall(b"ID=000094DA367B\x0d\x0aIP="+hostIP+b"\x0d\x0aPORT=50444\x0d\x0aCMD=CLOSE\x0d\x0a")
             cmd2.shutdown(socket.SHUT_WR)
             cmd2.close()
             return
@@ -47,43 +49,30 @@ def receive(recv):
 
 
 
-host = "192.168.68.64"
 port = 104                   # The same port as used by the server
 cmd = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 cmd.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 cmd.bind(('0.0.0.0',50452))
-#cmd.connect((host, port))
 
 port = 104
 recv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 recv.bind(('0.0.0.0',50444))
 recv.listen(5)
-#recv.setblocking(0)
 
-
-
-#otterThread = start_new_thread(receive, (recv,))
 otterThread = Thread(target=receive, args=(recv, ))
 otterThread.start()
-#start_new_thread(receive1, ())
 
 def run_program():
         #while True:
         print("Sending...")
-        cmd.connect((host, port))
+        cmd.connect((sensorIP, port))
         #cmd.send(b"ID=000094DA367B\x0d\x0aIP=192.168.68.1\x0d\x0aPORT=50444\x0d\x0aCMD=STATUS\x0d\x0a")
-        cmd.sendall(b"ID=000094DA367B\x0d\x0aIP=192.168.68.1\x0d\x0aPORT=50444\x0d\x0aCMD=CAPTURE\x0d\x0aMODE=0\x0d\x0a")
+        cmd.sendall(b"ID=000094DA367B\x0d\x0aIP="+hostIP+b"\x0d\x0aPORT=50444\x0d\x0aCMD=CAPTURE\x0d\x0aMODE=0\x0d\x0a")
         #cmd.sendall(b"ID=000094DA367B\x0d\x0aIP=192.168.68.1\x0d\x0aPORT=50444\x0d\x0aCMD=CLOSE\x0d\x0a")
         cmd.shutdown(socket.SHUT_WR)
         cmd.close()
         otterThread.join()
 
-        #cmd.shutdown(socket.SHUT_WR)
-        #cmd.close()
-    #s.sendall(b"ID=000094DA367B\x0d\x0aIP=192.168.68.1\x0d\x0aPORT=50444\x0d\x0aCMD=CAPTURE\x0d\x0aMODE=0\x0d\x0a")
-    #data = s.recv(1024)
-
-    #print('Received', repr(data))
 
 def exit_gracefully(signum, frame):
     # restore the original signal handler as otherwise evil things will happen
@@ -96,7 +85,7 @@ def exit_gracefully(signum, frame):
     cmd2.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     cmd2.bind(('0.0.0.0',50452))
     cmd2.connect((host, port))
-    cmd2.sendall(b"ID=000094DA367B\x0d\x0aIP=192.168.68.1\x0d\x0aPORT=50444\x0d\x0aCMD=CLOSE\x0d\x0a")
+    cmd2.sendall(b"ID=000094DA367B\x0d\x0aIP="+hostIP+b"\x0d\x0aPORT=50444\x0d\x0aCMD=CLOSE\x0d\x0a")
     cmd2.shutdown(socket.SHUT_WR)
     cmd2.close()
     print("bye.")
